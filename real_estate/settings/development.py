4535070d896723fb19759ff8bf87df9db2acb896
Vishal Patel <patel221007@gmail.com>
2023-08-05 00:20:07 +0530

    split settings

---
+++ real_estate/settings/development.py
@@ -0,0 +1,21 @@
+from .base import *
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': env("POSTGRES_ENGINE"),
+        'NAME': env("POSTGRES_DB"),
+        'USER': env("POSTGRES_USER"),
+        'PASSWORD': env("POSTGRES_PASSWORD"),
+        'HOST': env("PG_HOST"),   # Set to empty string for default.
+        'PORT': '',            # Leave empty to use the default port.
+    }
+}
